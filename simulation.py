2f5f1d5a2ef1ba83b3e1076a49ec3115c99f5db4
Emilio Cartoni <emilio.cartoni@yahoo.it>
2023-12-30 14:59:44 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -4,7 +4,11 @@ import random
 seed = random.randint(0, 1000000000)
 
 print("Application start")
-time.sleep(60) # Do some processing, like a long simulation
+# time.sleep(600) # Do some processing, like a long simulation
+start = time.time()
+while (time.time() - start < 610):
+    time.sleep(0.0001)
+
 
 with open(f"results_{seed}", 'w') as result_file:
     result_file.write("Some data\n")
